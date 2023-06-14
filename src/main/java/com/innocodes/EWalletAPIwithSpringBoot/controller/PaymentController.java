79e3844d8093bfbd796b2c672ed6de33ce5a28f9
InnocentOmenka <innocent.omenka@decagon.dev>
2023-06-14 16:55:58 +0100

    fixed dependencies issues

--- src/main/java/com/innocodes/EWalletAPIwithSpringBoot/controller/PaymentController.java
+++ src/main/java/com/innocodes/EWalletAPIwithSpringBoot/controller/PaymentController.java
@@ -1,5 +1,8 @@
 package com.innocodes.EWalletAPIwithSpringBoot.controller;
 
+import com.innocodes.EWalletAPIwithSpringBoot.dtos.paystack.AccountDto;
+import com.innocodes.EWalletAPIwithSpringBoot.dtos.paystack.BankDto;
+import com.innocodes.EWalletAPIwithSpringBoot.dtos.paystack.FundTransferDto;
 import com.innocodes.EWalletAPIwithSpringBoot.dtos.paystack.InitiateTransactionDto;
 import com.innocodes.EWalletAPIwithSpringBoot.dtos.responseDto.ApiResponse;
 import com.innocodes.EWalletAPIwithSpringBoot.dtos.responseDto.TransactionResponseDto;
@@ -11,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -56,6 +61,38 @@ public class PaymentController {
             @PathVariable String payment_reference) {
 
         return ResponseEntity.ok(paymentServices. verifyTransaction(payment_reference));
+    }
+
+    //   ====================== Transfer transactions =====================
+
+    @Operation(summary = "Confirm the account details before making transfers")
+    @GetMapping("/validate-account-details")
+    public ResponseEntity<ApiResponse<AccountDto>> validateAccountDetails(
+            @Parameter(description = "10 digit account number") @RequestParam(name = "account_number") String accountNumber,
+            @Parameter(description = "011 for first bank, 044 - access bank, etc") @RequestParam(name = "bank_code") String bankCode) {
+
+        return ResponseEntity.ok().body(paymentServices.getAccountDetails(accountNumber, bankCode));
+    }
+
+    @Operation(summary = "Fetch list of supported bank that are active in a country")
+    @GetMapping("/banks")
+    public ResponseEntity<ApiResponse<List<BankDto>>> fetchAllBanks(@Parameter(description = "NGN for Nigeria, GHS for Ghana etc") @RequestParam(name = "currency") String currency,
+                                                                    @Parameter(description = "(Optional) enter 'nuban' or 'mobile_money', etc") @RequestParam(name = "type", required = false) String type) {
+        return ResponseEntity.ok(paymentServices.fetchBanks(currency, type != null && !type.equals("") ? type : null));
+    }
+
+    @Operation(summary = "Before sending money to an account, you need to create a transfer recipient with the customer’s account details.")
+    @PostMapping("/withdrawal/create-transfer-recipient")
+    public ResponseEntity<ApiResponse<FundTransferDto>> createTransferRecipient(@RequestBody AccountDto accountDto) {
+
+        return ResponseEntity.ok(paymentServices.createTransferRecipient(accountDto));
+    }
+
+    @Operation(summary = "Initiate the transfer")
+    @PostMapping("/withdrawal/initiate")
+    public ResponseEntity<ApiResponse<TransactionResponseDto>> initiateTransfer(@RequestBody FundTransferDto fundTransferDto) {
+
+        return ResponseEntity.ok(paymentServices.initiateTransfer(fundTransferDto));
     }
 
 }
